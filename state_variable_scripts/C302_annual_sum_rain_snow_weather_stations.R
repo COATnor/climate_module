1ab8f4dd048f2dede3c21782159352a9f813413a
gurobsynnes <guro.b.synnes@uit.no>
2026-07-09 16:46:29 +0200

    Update C302_annual_sum_rain_snow_weather_stations.R

--- state_variable_scripts/C302_annual_sum_rain_snow_weather_stations.R
+++ state_variable_scripts/C302_annual_sum_rain_snow_weather_stations.R
@@ -9,7 +9,7 @@
 ## ------------------------------------------------------------------ ##
 # This script calculates the annual sum of precipitation and the fraction falling as rain and snow for weather stations.
 # It also generates aux and coordinate files containing information about when stations have been in use and their coordinates, respectively.
-# It downloads the elements sum(precipitation_amount P1Y) with timeOffset PT6H, and mean(air_temperature P1D) from Frost with timeOffset PT0H (preferred) or PT6H.
+# From Frost, it downloads the elements sum(precipitation_amount P1Y) with timeOffset PT6H, and mean(air_temperature P1D) with timeOffset PT0H (preferred) or PT6H.
 # The frs function (sourced from GitHub) is used to calculate the fractions of the precipitation falling as rain and snow based on temperatures,
 # but the annual sum of precipitation is taken directly from sum(precipitation_amount P1Y).
 
